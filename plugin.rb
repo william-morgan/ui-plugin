bcb7340f81043559ae9d5ad359e2270d2506859e
William Morgan <william.morgan@uwalumni.com>
2015-10-19 16:17:30 -0500

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -6,4 +6,5 @@ after_initialize do
   load File.expand_path("../models/category.rb", __FILE__)
   load File.expand_path("../models/category_list.rb", __FILE__)
   load File.expand_path("../serializers/category_detailed_serializer.rb", __FILE__)
+  load File.expand_path("../views/static/login.html.erb", __FILE__)
 end
