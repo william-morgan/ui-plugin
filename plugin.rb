4af10ae869ac0df1b3c31d4231b8b52439c3e559
William Morgan <william.morgan@uwalumni.com>
2018-09-14 19:07:55 -0600

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -2,7 +2,10 @@
 # about: adds extra subcategory information to the category list serializer
 # author: william morgan
 after_initialize do
+
   load File.expand_path("../models/category.rb", __FILE__)
   load File.expand_path("../models/category_list.rb", __FILE__)
+  load File.expand_path("../models/user.rb", __FILE__)
   load File.expand_path("../serializers/category_detailed_serializer.rb", __FILE__)
+  load File.expand_path("../serializers/user_serializer.rb", __FILE__)
 end
