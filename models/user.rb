d3efac36a560328380dd8b529d510a514327c490
William Morgan <william.morgan@uwalumni.com>
2018-09-15 09:52:32 -0600

    Update user.rb

--- models/user.rb
+++ models/user.rb
@@ -1,15 +1,20 @@
 User.class_eval do
-  attr_accessor :profile_fields
+  attr_accessor :user_post_data
   
-  def profile_fields
-    profile_fields(self.id)
+  def user_post_data
+    sql = (<<~SQL).freeze
+      SELECT likes_received AS reputation, 
+             to_char(users.created_at, 'Mon YYYY') AS join_date, 
+             post_count AS total_posts, 
+             date_part('day',now()- created_at) AS rep_power 
+      FROM users, user_stats WHERE users.id=user_stats.user_id AND users.id=:user_id LIMIT 1
+    SQL
+    DB.query(sql, user_id: id).map{ 
+      |q| {
+           total_posts: q.total_posts, 
+           rep_power: q.rep_power, 
+           join_date: q.join_date, 
+           reputation: q.reputation}
+      }[0]
   end
-  private
-    def profile_fields
-      @profile_fields[:join_date]  = User.where(id: id).pluck("to_char(created_at, 'Mon YYYY') AS created_at").first
-      @profile_fields[:post_count] = Post.where(user_id: id, hidden: false).count
-      @profile_fields[:reputation] = UserAction.where(user_id: id, action_type: UserAction::LIKE).count
-      @profile_fields[:rep_power]  =  User.where(:id=>2).pluck("date_part('day',now()- created_at)").first.to_i
-      @profile_fields
-    end
 end
