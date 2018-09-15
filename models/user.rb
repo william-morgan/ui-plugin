dc950fe62d3d295ae540ea7e09c476bc2bca4cc5
William Morgan <william.morgan@uwalumni.com>
2018-09-14 18:37:23 -0600

    Update user.rb

--- models/user.rb
+++ models/user.rb
@@ -4,9 +4,9 @@ User.class_eval do
   end
   private
     def profile_fields
-      @fields[:join_date]  = User.where(:id=>2).pluck("to_char(created_at, 'Mon YYYY') AS created_at").first
-      @fields[:post_count] = Post.where("user_id='?' AND hidden = FALSE", 2).count
-      @fields[:reputation] = User.where(:id=>2).joins(:user_stat).pluck(:likes_given).first
-      @fields[:rep_power]  =  User.where(:id=>2).pluck("date_part('day',now()- created_at)").first.to_i
+      @profile_fields[:join_date]  = User.where(:id=>2).pluck("to_char(created_at, 'Mon YYYY') AS created_at").first
+      @profile_fields[:post_count] = Post.where("user_id='?' AND hidden = FALSE", 2).count
+      @profile_fields[:reputation] = User.where(:id=>2).joins(:user_stat).pluck(:likes_given).first
+      @profile_fields[:rep_power]  =  User.where(:id=>2).pluck("date_part('day',now()- created_at)").first.to_i
     end
 end
