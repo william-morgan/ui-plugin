59c90f955d5cc00f448e8cf87b09bd29d05e6819
William Morgan <william.morgan@uwalumni.com>
2018-09-14 18:25:33 -0600

    Update user.rb

--- models/user.rb
+++ models/user.rb
@@ -7,5 +7,6 @@ User.class_eval do
       @fields[:join_date]  = User.where(:id=>2).pluck("to_char(created_at, 'Mon YYYY') AS created_at").first
       @fields[:post_count] = Post.where("user_id='?' AND hidden = FALSE", 2).count
       @fields[:reputation] = User.where(:id=>2).joins(:user_stat).pluck(:likes_given).first
+      @fields[:rep_power]  =  User.where(:id=>2).pluck("date_part('day',now()- created_at)").first.to_i
     end
 end
